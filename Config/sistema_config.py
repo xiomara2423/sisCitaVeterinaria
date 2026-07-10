11ca8e468eb62478be2e81d7c624d19e99a91f82
ricardofloresh-2567 <ricardo.floresh@istpargentina.edu.pe>
2026-07-10 15:55:41 -0500

    Carpeta Config (logger.py, persistencia.py, sistema_config.py) añadida al repositorio

---
+++ Config/sistema_config.py
@@ -0,0 +1,44 @@
+from Config.logger import Logger
+
+# =======================================================================================
+#  SINGLETON: SistemaConfig
+# =======================================================================================
+class SistemaConfig:
+    _inst = None
+
+    def __new__(cls):
+        if cls._inst is None:
+            cls._inst = super().__new__(cls)
+            cls._inst.nombre  = "SisCita Veterinaria RX"
+            cls._inst.version = "1.0"
+            cls._inst.empresa = "IESTP Argentina"
+            cls._inst.autor   = "Xiomara Espinoza/Ricardo Flores"
+            Logger().info(
+                f"Sistema Iniciado: {cls._inst.nombre} | "
+                f"Version: {cls._inst.version} | "
+                f"Empresa: {cls._inst.empresa} | "
+                f"Autor: {cls._inst.autor}"
+            )
+        return cls._inst
+
+# =======================================================================================
+#  EXCEPCIONES
+# =======================================================================================
+class DuenoNoEncontradoError(Exception):
+    def __init__(self, id): super().__init__(f"Dueño ID = {id} no encontrado")
+
+class EmailDuplicadoError(Exception):
+    def __init__(self, email): super().__init__(f"Email '{email}' ya registrado")
+
+class MascotaNoEncontradaError(Exception):
+    def __init__(self, id): super().__init__(f"Mascota ID = {id} no encontrada")
+
+class VeterinarioNoEncontradoError(Exception):
+    def __init__(self, id): super().__init__(f"Veterinario ID = {id} no encontrado")
+
+class CitaNoEncontradaError(Exception):
+    def __init__(self, id): super().__init__(f"Cita ID = {id} no encontrada")
+    
+class EstadoInvalidoError(Exception):
+    def __init__(self, estado, validos):
+        super().__init__(f"Estado inválido '{estado}'. Use uno de: {validos}")
