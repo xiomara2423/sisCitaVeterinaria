e692fa97ff855d79d5aa31d748beb3f88d47923e
Xiomara Espinoza <xiomara.espinozaq@istpargentina.edu.pe>
2026-07-22 00:15:06 -0500

    Actualizando base_datos.py aplicando postgreSQL

--- sisCitaVeterinaria/Config/base_datos.py
+++ sisCitaVeterinaria/Config/base_datos.py
@@ -1,72 +1,81 @@
-import sqlite3
+import os 
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 ARCHIVO_BD = "veterinaria.db"
 
 def obtener_conexion():
-    conn = sqlite3.connect(ARCHIVO_BD)
-    #Acceder filas como diccionarios en lugar de tuplas
-    conn.row_factory = sqlite3.Row
-    #Aplicar integridad de llaves foraneas
-    conn.execute("PRAGMA foreign_keys = ON")
-    return conn
-
+    
+    #CREDENCIALES 
+    conn = psycopg2.connect(
+        host=os.getenv("BD_HOST", "localhost"),
+        port=os.getenv("BD_PORT", "5432"),
+        database=os.getenv("BD_NAME", "sistema_db"),
+        user=os.getenv("BD_USER", "postgres"),
+        password=os.getenv("BD_PASSWORD", "123456")
+    )
+    
+    conn.cursor_factory=RealDictCursor
+    return conn 
 def inicializar():
+    # Crea las tablas si aún no existen en PostgreSQL
     conn = obtener_conexion()
     cursor = conn.cursor()
 
-    #Tabla duenos
+    # Tabla dueños
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS duenos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nombre TEXT NOT NULL,
-            apellido TEXT NOT NULL,
-            telefono TEXT NOT NULL,
-            email TEXT UNIQUE,
-            direccion TEXT NOT NULL,
-            fecha_registro TEXT DEFAULT CURRENT_TIMESTAMP
+            id              SERIAL          PRIMARY KEY,
+            nombre          TEXT            NOT NULL,
+            apellido        TEXT            NOT NULL,
+            telefono        TEXT            NOT NULL,
+            email           TEXT            UNIQUE,
+            direccion       TEXT            NOT NULL,
+            fecha_registro  TEXT            DEFAULT TO_CHAR(CURRENT_TIMESTAMP, 'DD-MM-YYYY HH24:MI:SS')
         )
     """)
 
-    #Tabla mascotas
+    # Tabla mascotas
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS mascotas (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            dueno_id INTEGER NOT NULL,
-            nombre TEXT NOT NULL,
-            especie TEXT NOT NULL,
-            raza TEXT,
-            sexo TEXT CHECK (sexo IN ('M', 'H')),
-            peso REAL,
+            id          SERIAL      PRIMARY KEY,
+            dueno_id    INTEGER     NOT NULL,
+            nombre      TEXT        NOT NULL,
+            especie     TEXT        NOT NULL,
+            raza        TEXT,
+            sexo        TEXT        CHECK (sexo IN ('M', 'H')),
+            peso        REAL,
             FOREIGN KEY (dueno_id) REFERENCES duenos(id)
         )
     """)
 
-    #Tabla veterinarios
+    # Tabla veterinarios
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS veterinarios (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nombre TEXT NOT NULL,
-            apellido TEXT NOT NULL,
-            especialidad TEXT NOT NULL,
-            telefono TEXT,
-            disponible INTEGER DEFAULT 1
+            id              SERIAL      PRIMARY KEY,
+            nombre          TEXT        NOT NULL,
+            apellido        TEXT        NOT NULL,
+            especialidad    TEXT        NOT NULL,
+            telefono        TEXT,
+            disponible      INTEGER     DEFAULT 1
         )
     """)
 
-    #Tabla citas
+    # Tabla citas
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS citas (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            mascota_id INTEGER NOT NULL,
-            veterinario_id INTEGER NOT NULL,
-            fecha TEXT NOT NULL,
-            motivo TEXT NOT NULL,
-            estado TEXT DEFAULT 'Programada' NOT NULL
-                CHECK (estado IN ('Programada', 'Completada', 'Cancelada')),
+            id              SERIAL      PRIMARY KEY,
+            mascota_id      INTEGER     NOT NULL,
+            veterinario_id  INTEGER     NOT NULL,
+            fecha           TEXT        NOT NULL,
+            motivo          TEXT        NOT NULL,
+            estado          TEXT        DEFAULT 'Programada' NOT NULL
+                                        CHECK (estado IN ('Programada', 'Completada', 'Cancelada')),
             FOREIGN KEY (mascota_id) REFERENCES mascotas(id),
             FOREIGN KEY (veterinario_id) REFERENCES veterinarios(id)
         )
     """)
 
+    # Confirma y guarda los cambios en PostgreSQL
     conn.commit()
     conn.close()
